670cfbcda502810b815a652d73822b436168e9f0
Svetlana <bel-svetik@yandex.ru>
2024-12-18 17:59:31 +0300

    Пипетка для выбора цвета с холста

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import colorchooser, filedialog, messagebox
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageTk
+from settings import *
+from utilities import image_to_icon
 
 
 class DrawingApp:
@@ -8,13 +10,30 @@ class DrawingApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Рисовалка с сохранением в PNG")
+        self.root.minsize(610, 450)
+        try:
+            self.root.iconbitmap(default="./icon/favicon.ico")
+            # TODO Для компиляции с помощью auto-py-to-exe заменить строку выше на:
+            # self.root.iconbitmap(default=path.join(sys._MEIPASS, "./icon/favicon.ico"))
+        except:
+            pass
 
         self.bg_color = 'white'
-        self.image = Image.new("RGB", (600, 400), color=self.bg_color)
+        self.width = 600
+        self.height = 400
+        self.image = Image.new("RGB", (self.width, self.height), color=self.bg_color)
         self.draw = ImageDraw.Draw(self.image)
 
-        self.canvas = tk.Canvas(root, width=600, height=400, bg=self.bg_color)
-        self.canvas.pack()
+        self.icon_save = image_to_icon(ICON_SAVE)
+        self.icon_insert = image_to_icon(ICON_INSERT)
+        self.icon_new = image_to_icon(ICON_NEW)
+        self.icon_brash = image_to_icon(ICON_BRASH)
+        # self.icon_pipette = image_to_icon(ICON_PIPETTE)
+        self.icon_palette = image_to_icon(ICON_PALETTE)
+        self.icon_eraser = image_to_icon(ICON_ERASER)
+
+        self.canvas = tk.Canvas(root, width=self.width, height=self.height, bg=self.bg_color)
+        self.canvas.pack(expand=True)
 
         self.setup_ui()
 
@@ -23,6 +42,7 @@ class DrawingApp:
         self.pen_color_save = self.pen_color
 
         self.canvas.bind('<B1-Motion>', self.paint)
+        self.canvas.bind('<Button-3>', self.pick_color)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
 
     def set_brush(self, choice):
@@ -31,32 +51,39 @@ class DrawingApp:
 
     def setup_ui(self):
         """  Виджеты управления  """
-        control_frame = tk.Frame(self.root)
-        control_frame.pack(fill=tk.X)
+        control_frame = tk.Frame(self.root, relief=tk.RAISED, border=2)
+        control_frame.pack(fill=tk.BOTH)
 
-        clear_button = tk.Button(control_frame, text="Очистить", command=self.clear_canvas)
-        clear_button.pack(side=tk.LEFT)
-
-        color_button = tk.Button(control_frame, text="Выбрать цвет", command=self.choose_color)
-        color_button.pack(side=tk.LEFT)
-
-        pen_button = tk.Button(control_frame, text="Кисть", command=self.pen_image)
-        pen_button.pack(side=tk.LEFT)
-
-        eraser_button = tk.Button(control_frame, text="Ластик", command=self.eraser_image)
-        eraser_button.pack(side=tk.LEFT)
-
-        save_button = tk.Button(control_frame, text="Сохранить", command=self.save_image)
+        save_button = tk.Button(control_frame, image=self.icon_save, text="Сохранить", command=self.save_image)
         save_button.pack(side=tk.LEFT)
 
-        self.brush_size_scale = tk.Scale(control_frame, from_=1, to=20, orient=tk.HORIZONTAL)
+        save_button = tk.Button(control_frame, image=self.icon_insert, text="Вставить", command=self.image_insert)
+        save_button.pack(side=tk.LEFT)
+
+        clear_button = tk.Button(control_frame, image=self.icon_new, text="Очистить", command=self.clear_canvas)
+        clear_button.pack(side=tk.LEFT)
+
+        color_button = tk.Button(control_frame, image=self.icon_palette, text="Палитра", command=self.choose_color)
+        color_button.pack(side=tk.LEFT)
+
+        # pipette_button = tk.Button(control_frame, image=self.icon_pipette, text="Пипетка", command=self.choose_color)
+        # pipette_button.pack(side=tk.LEFT)
+
+        pen_button = tk.Button(control_frame, image=self.icon_brash, text="Кисть", command=self.pen_image)
+        pen_button.pack(side=tk.LEFT)
+
+        eraser_button = tk.Button(control_frame, image=self.icon_eraser, text="Ластик", command=self.eraser_image)
+        eraser_button.pack(side=tk.LEFT)
+
+        self.brush_size_scale = tk.Scale(control_frame, from_=5, to=20, orient=tk.HORIZONTAL)
         self.brush_size_scale.pack(side=tk.LEFT)
 
         sizes = [1, 2, 5, 10, 20]
         variable = tk.StringVar()
-        variable.set(str(sizes[4]))
+        variable.set(str(sizes[2]))
         self.menu_brush = tk.OptionMenu(control_frame, variable, *sizes, command=self.set_brush)
         self.menu_brush.pack(side=tk.LEFT)
+
 
     def paint(self, event):
         """
@@ -89,7 +116,7 @@ class DrawingApp:
         Очищает холст, удаляя все нарисованное, и пересоздает объекты Image и ImageDrawдля нового изображения.
         """
         self.canvas.delete("all")
-        self.image = Image.new("RGB", (600, 400), "white")
+        self.image = Image.new("RGB", (self.width, self.height), "white")
         self.draw = ImageDraw.Draw(self.image)
 
     def choose_color(self):
@@ -97,6 +124,13 @@ class DrawingApp:
         Открывает стандартное диалоговое окно выбора цвета и устанавливает выбранный цвет как текущий для кисти.
         """
         self.pen_color = colorchooser.askcolor(color=self.pen_color)[1]
+        self.pen_color_save = self.pen_color
+
+    def pick_color(self, event):
+        """
+        Инструмент в виде пипетки для выбора цвета.
+        """
+        self.pen_color = "#{:02X}{:02X}{:02X}".format(*self.image.getpixel((event.x, event.y)))
         self.pen_color_save = self.pen_color
 
     def pen_image(self):
@@ -122,6 +156,18 @@ class DrawingApp:
                 file_path += '.png'
             self.image.save(file_path)
             messagebox.showinfo("Информация", "Изображение успешно сохранено!")
+
+    def image_insert(self):
+        """
+        Вставить картинку из файла.
+        """
+        image = Image.open(filedialog.askopenfile(filetypes=[("JPG files", ".jpg"), ("PNG files", ".png")]).name)
+        self.photo = ImageTk.PhotoImage(image)
+        # self.image.paste(self.photo, (0, 0, 600, 400))
+        # for x in range(self.photo.width()):
+        #     for y in range(self.photo.height()):
+        #         self.image.putpixel((x, y), self.photo.(x, y))
+        self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
 
 def main():
---
+++ settings.py
@@ -0,0 +1,17 @@
+# Иконки кнопок и пунктов меню
+ICON_NEW = './icon/new.png'
+ICON_BRASH = './icon/brash.png'
+ICON_ERASER = './icon/eraser.png'
+ICON_PIPETTE = './icon/pipette.png'
+ICON_PALETTE = './icon/palette.png'
+ICON_SAVE = './icon/save.png'
+ICON_INSERT = './icon/insert.png'
+
+# TODO Для компиляции с помощью auto-py-to-exe раскомментировать импорты и заменить строки выше на:
+# ICON_NEW = path.join(sys._MEIPASS, './icon/new.png')
+# ICON_BRASH = path.join(sys._MEIPASS, './icon/brash.png')
+# ICON_ERASER = path.join(sys._MEIPASS, './icon/eraser.png')
+# ICON_PIPETTE = path.join(sys._MEIPASS, './icon/pipette.png')
+# ICON_PALETTE = path.join(sys._MEIPASS, './icon/palette.png')
+# ICON_SAVE = path.join(sys._MEIPASS, './icon/save.png')
+# ICON_INSERT = path.join(sys._MEIPASS, './icon/insert.png')
